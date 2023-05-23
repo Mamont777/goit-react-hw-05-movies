3e985b27dab7a9899c136387e10a9a683956ad21
Sergii Podobrii <podobri@ukr.net>
2023-05-23 17:17:33 +0300

    амитмими

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,14 +1,28 @@
 //
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import { getMovieByName } from '../../services/themoviedbAPI.js';
 import MovieList from '../../components/MovieList/MovieList';
 import Loader from '../../components/Loader/Loader';
 import Search from '../../components/SearchForm/Search.jsx';
 
+const notifyOptions = {
+  position: 'top-center',
+  autoClose: 1500,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  theme: 'colored',
+};
+
 const Movies = () => {
   const [foundFilms, setFoundFilms] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [showNotification, setShowNotification] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
   const searchQuery = searchParams.get('query');
 
@@ -20,10 +34,14 @@ const Movies = () => {
         setIsLoading(true);
         const data = await getMovieByName(searchQuery);
         setFoundFilms(data.results);
+        if (data.results.length === 0) {
+          setShowNotification(true);
+        }
       } catch (error) {
         console.log(error.message);
       } finally {
         setIsLoading(false);
+        setShowNotification(false);
       }
     }
     fetchMovies();
@@ -38,6 +56,10 @@ const Movies = () => {
       {isLoading && <Loader />}
       <Search updateQueryString={updateQueryString} />
       {foundFilms.length > 0 && <MovieList movies={foundFilms} />}
+      {foundFilms.length === 0 &&
+        showNotification &&
+        toast.error('No movies found with such title', notifyOptions)}
+      <ToastContainer />
     </>
   );
 };
